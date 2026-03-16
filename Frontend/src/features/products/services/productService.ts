60deae4a14624d11212958749e7ef8f7acebf4c9
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2026-03-16 09:34:03 +0100

    fixed typo in import

--- Frontend/src/features/products/services/productService.ts
+++ Frontend/src/features/products/services/productService.ts
@@ -1,9 +1,17 @@
 import type { AxiosInstance } from "axios";
 import type { ProductType } from "../types/productType";
+import { ProductSchema } from "../schemas/productSchema";
 
 export const createProductService = (api: AxiosInstance) => ({
     getProducts: async () => {
         const res = await api.get<ProductType[]>("/api/Product/GetAll");
-        return res.data;
+        const parsed = ProductSchema.safeParse(res.data);
+
+        if (!parsed.success) {
+            console.error("Invalid product data", parsed.error);
+            return null;
+        }
+
+        return parsed.data;
     }
 })
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import './shared/style/index.css'
-import Providers from './app/providers.tsx'
+import Providers from './app/Providers.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
