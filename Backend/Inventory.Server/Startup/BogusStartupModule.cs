db1efb661d7ef42244d45379bd3d42c1da6c5320
kristensenmorten <kristensen.morten+github@gmail.com>
2026-03-18 10:03:10 +0100

    #74 OrderItems unique plus more data generated

--- Backend/Inventory.Server/Startup/BogusStartupModule.cs
+++ Backend/Inventory.Server/Startup/BogusStartupModule.cs
@@ -44,31 +44,35 @@ public static class FakeData
     public static IEnumerable<Category> GetCategories()
     {
         var categoryFaker = new Faker<Category>()
+            .UseSeed(12121)
             .RuleFor(c => c.Name, f => f.Commerce.Department());
-        return categoryFaker.Generate(5);
+        return categoryFaker.Generate(10);
     }
 
 
     internal static IEnumerable<Location> GetLocations()
     {
         var locationFaker = new Faker<Location>()
+            .UseSeed(23232)
             .RuleFor(l => l.Name, f => f.Company.CompanyName());
-        return locationFaker.Generate(5);
+        return locationFaker.Generate(10);
     }
 
     internal static IEnumerable<Product> GetProducts(IEnumerable<Category> categories)
     {
         var productFaker = new Faker<Product>()
+            .UseSeed(34343)
             .RuleFor(p => p.Category, f => f.PickRandom(categories))
             .RuleFor(p => p.Name, f => f.Commerce.ProductName())
             .RuleFor(p => p.Price, f => decimal.Parse(f.Commerce.Price()))
             ;
-        return productFaker.Generate(25);
+        return productFaker.Generate(100);
     }
 
     internal static IEnumerable<LocationItem> GetLocationItems(List<Location> locations, List<Product> products)
     {
         var locationItemFaker = new Faker<LocationItem>()
+            .UseSeed(45454)
             .RuleFor(li => li.Quantity, f => f.Random.Number(0, 1000))
             .RuleFor(li => li.TargetQuantity, f => f.Random.Number(100, 500))
             .RuleFor(li => li.ReservedQuantity, f => f.Random.Number(0, 50));
@@ -76,10 +80,10 @@ public static class FakeData
         {
             foreach(var product in products)
             {
-                var x = locationItemFaker.Generate(1).First();
-                x.Location = location;
-                x.Product = product;
-                yield return x;
+                var locationItem = locationItemFaker.Generate(1).First();
+                locationItem.Location = location;
+                locationItem.Product = product;
+                yield return locationItem;
             }
         }
     }
@@ -87,23 +91,39 @@ public static class FakeData
     internal static IEnumerable<Order> GetOrders(List<Location> locations)
     {
         var orderFaker = new Faker<Order>()
+            .UseSeed(56565)
             .RuleFor(o => o.Status, f => f.PickRandom<OrderStatus>())
             .RuleFor(o => o.ReferenceId, f => Guid.NewGuid())
             //Perhaps remove the OrNull?
             .RuleFor(o => o.Location, f => f.PickRandom(locations).OrNull(f))
             ;
 
-        return orderFaker.Generate(5);
+        return orderFaker.Generate(15);
     }
-
 
     internal static IEnumerable<OrderItem> GetOrderItems(List<Order> orders, List<Product> products)
     {
         var orderItemFaker = new Faker<OrderItem>()
-            .RuleFor(oi => oi.Product, f => f.PickRandom(products))
-            .RuleFor(oi => oi.Order, f => f.PickRandom(orders))
+            .UseSeed(67676)
             .RuleFor(oi => oi.Quantity, f => f.Random.Number(1, 10));
+        var rng = new Random(78787);
 
-        return orderItemFaker.Generate(30);
+        foreach (var order in orders)
+        {
+            var noOfItems = rng.Next(1, 10);
+            var productsInOrder = Enumerable
+                .Range(0, noOfItems)
+                .Select(x => rng.Next(0, 1 + products.Count - noOfItems))
+                .OrderBy(x => x)
+                .Select((x, i) => products[x + i]);
+
+            foreach (var product in productsInOrder)
+            {
+                var orderItem = orderItemFaker.Generate(1).First();
+                orderItem.Order = order;
+                orderItem.Product = product;
+                yield return orderItem;
+            }
+        }
     }
 }
