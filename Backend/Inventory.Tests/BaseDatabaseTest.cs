b78721bad238835c38bb7e8b17c40ff4a758fcc9
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-18 09:40:01 +0100

    refactor: adds cleanup of database to BaseDatabaseTest and changes it to make a factory instead of context.

--- Backend/Inventory.Tests/BaseDatabaseTest.cs
+++ Backend/Inventory.Tests/BaseDatabaseTest.cs
@@ -1,24 +1,52 @@
 using Inventory.Persistence;
 using Inventory.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Npgsql;
 
 namespace Inventory.Tests;
 
 public abstract class BaseDatabaseTest
 {
-    private const string TEST_DATABASE = "inventory_test";
-    protected InventoryDatabaseContext context;
-    private ConfigurationService configurationService;
+    private static readonly string TestDatabaseName = $"inventory_test_{Guid.NewGuid():N}";
+    private IDbContextFactory<InventoryDatabaseContext> contextFactory;
+    private readonly ConfigurationService configurationService;
+    private bool disposed;
+
     protected BaseDatabaseTest()
     {
         configurationService = new ConfigurationService();
         _ = configurationService.BuildConfiguration();
 
         var optionsBuilder = new DbContextOptionsBuilder<InventoryDatabaseContext>();
-        var connectionString = ReplaceDatabase(configurationService.GetConnectionString(), TEST_DATABASE);
+
+        var connectionString = ReplaceDatabase(configurationService.GetConnectionString(), TestDatabaseName);
+
         configurationService.ConfigureDatabaseOptions(optionsBuilder, connectionString);
-        context = new InventoryDatabaseContext(optionsBuilder.Options);
+
+        contextFactory = new PooledDbContextFactory<InventoryDatabaseContext>(optionsBuilder.Options);
+
+        using var context = CreateContext();
+        context.Database.Migrate();
+    }
+
+    protected InventoryDatabaseContext CreateContext()
+    {
+        return contextFactory.CreateDbContext();
+    }
+
+    public void Dispose()
+    {
+        if (disposed)
+        {
+            return;
+        }
+
+        using var context = CreateContext();
+        context.Database.EnsureDeleted();
+
+        disposed = true;
+        GC.SuppressFinalize(this);
     }
 
     private string ReplaceDatabase(string connectionString, string testDatabaseName)
