3a21472fa98b724b9921fb946442d2686cebfeba
kristensenmorten <kristensen.morten+github@gmail.com>
2026-03-18 10:15:38 +0100

    #74 enabled unique constraints and migration added

---
+++ Backend/Inventory.Persistence/Migrations/20260318091332_AddUniqueness.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocationItems_LocationId",
+                table: "LocationItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId_ProductId",
+                table: "OrderItems",
+                columns: new[] { "OrderId", "ProductId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationItems_LocationId_ProductId",
+                table: "LocationItems",
+                columns: new[] { "LocationId", "ProductId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_OrderId_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocationItems_LocationId_ProductId",
+                table: "LocationItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationItems_LocationId",
+                table: "LocationItems",
+                column: "LocationId");
+        }
+    }
+}
