21d9d311ac5cceea2aa0f26c396ab50dd8796e15
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-18 12:26:04 +0100

    chore: creates CategoryQueryServiceTests with empty tests

---
+++ Backend/Inventory.Tests/CategoryQueryServiceTests.cs
@@ -0,0 +1,149 @@
+using Inventory.Tests.Core;
+
+namespace Inventory.Tests;
+
+public class CategoryQueryServiceTests : BaseDatabaseTest
+{
+    [Test]
+    public async Task GetEntity_WithMatchingName_ReturnsCategory()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntity_WithName_IsCaseInsensitive()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntity_WithNonMatchingName_ReturnsNull()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntities_WithMatchingName_ReturnsAllMatches()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntities_WithNoMatches_ReturnsEmptyCollection()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetAllEntities_WhenCategoriesExist_ReturnsAllCategories()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetAllEntities_WhenNoCategoriesExist_ReturnsEmptyCollection()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task AddEntity_PersistsCategory()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task UpdateEntity_UpdatesCategoryName()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task DeleteEntity_RemovesCategory()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task DeleteEntityById_RemovesCategory()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntityComplex_ThrowsNotImplementedException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetEntitiesComplex_ThrowsNotImplementedException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        throw new NotImplementedException();
+    }
+}
