f22138152b0e703c1e21260556218be7300a6225
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-20 09:16:26 +0100

    refactor: makes use of CreateOrderItemDto when creating an Order

--- Backend/Inventory.Model/Dto/Create/CreateOrderDto.cs
+++ Backend/Inventory.Model/Dto/Create/CreateOrderDto.cs
@@ -8,11 +8,11 @@ public class CreateOrderDto
 {
     public Guid Reference { get; set; } = Guid.NewGuid();
     // Todo: Change OrderItem to a CreateOrderItemDto
-    public required IEnumerable<OrderItem> Items { get; set; }
+    public required IEnumerable<CreateOrderItemDto> Items { get; set; }
     int? PreferredLocationId { get; set; }
 
     public async Task<IList<IOrder>> UnpackDto(IOrderService service)
     {
-        return await service.CreateOrders(Reference, Items, PreferredLocationId);
+        return await service.CreateOrders(Reference, Items.Select(x => x.UnpackDto()), PreferredLocationId);
     }
 }
